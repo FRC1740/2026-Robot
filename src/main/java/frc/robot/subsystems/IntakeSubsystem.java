90ba6b7a13c873f140e4f5423b8311fd3b49767f
Nikolas Daynard <Nkdaynard@gmail.com>
2026-02-20 17:09:00 -0500

    new reverse limit

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -46,7 +46,7 @@ public class IntakeSubsystem extends SubsystemBase {
     config.softLimit.reverseSoftLimitEnabled(true);
     config.encoder.positionConversionFactor(3);
     config.softLimit.forwardSoftLimit(0);
-    config.softLimit.reverseSoftLimit(-100);
+    config.softLimit.reverseSoftLimit(-133.33334);
     config.idleMode(IdleMode.kBrake);
     
     extensionMotorController.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
