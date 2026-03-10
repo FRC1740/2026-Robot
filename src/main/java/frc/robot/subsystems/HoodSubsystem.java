b09be7e7c55f4f52a9cc5d49596e8c5f57cc9abd
Nikolas Daynard <Nkdaynard@gmail.com>
2026-03-10 16:52:59 -0400

    fix hoo

--- src/main/java/frc/robot/subsystems/HoodSubsystem.java
+++ src/main/java/frc/robot/subsystems/HoodSubsystem.java
@@ -56,7 +56,7 @@ public class HoodSubsystem extends SubsystemBase {
 
   private void set(double angle, double angle2) {
     servo.set(angle);
-    servo2.set(angle2 - .05);
+    servo2.set((angle2 - .05) - 0.3);
   }
 
   private void setAngle(double degrees, Servo the_servo) {
