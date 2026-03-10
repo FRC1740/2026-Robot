a071e3d2adae5da62a677e8479aeac7ee61b01a5
Nikolas Daynard <Nkdaynard@gmail.com>
2026-03-10 17:01:50 -0400

    tune shooter

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -72,14 +72,15 @@ public class ShooterSubsystem extends SubsystemBase {
     // Without this the motor draws as much power as it wants and will die if stalled
     motorConfig.CurrentLimits.StatorCurrentLimit = 50;
     motorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
-    motorConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 1;
+    motorConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = .5;
     motorConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
 
     // PID
     
-    slot0Configs.kP = 3.5; // An error of 1 rotation results in 2.4 V output
+    slot0Configs.kP = 7; // An error of 1 rotation results in 2.4 V output
     slot0Configs.kI = 0; // no output for integrated error
     slot0Configs.kD = 0; // A velocity of 1 rps results in 0.1 V output
+    slot0Configs.kS = 5;
 
     slot1Configs.kP = 0;
     slot1Configs.kI = 0;
