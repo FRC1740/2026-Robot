7944e25787c68a0c74434b7bc07bd5fb0dfa730a
Nikolas Daynard <Nkdaynard@gmail.com>
2026-03-24 18:14:24 -0400

    merge

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -85,7 +85,7 @@ public class ShooterSubsystem extends SubsystemBase {
   
     // PID
     
-    slot0Configs.kP = 10; // An error of 1 rotation results in 2.4 V output
+    slot0Configs.kP = 10; // An error of 1 rotation results in 10 V output
     slot0Configs.kI = 0; // no output for integrated error
     slot0Configs.kD = 0; // A velocity of 1 rps results in 0.1 V output
     slot0Configs.kS = 5;
