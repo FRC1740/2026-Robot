4b5139f0b1b4f7fc7cccd5c03938eff206a12aca
Nikolas Daynard <Nkdaynard@gmail.com>
2026-06-04 01:11:49 -0400

    auto align on the move estimates

--- src/main/java/frc/robot/commands/Align.java
+++ src/main/java/frc/robot/commands/Align.java
@@ -9,12 +9,17 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
+import frc.robot.Constants.VisionConstants;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
@@ -23,7 +28,7 @@ public class Align extends Command {
   CommandSwerveDrivetrain m_drivetrain;
   SwerveRequest.FieldCentric m_drive;
   CommandXboxController m_driverController;
-  Transform2d target;
+  Pose2d target;
 
   Pose2d targetHub;
 
@@ -39,6 +44,11 @@ public class Align extends Command {
   private GenericEntry target_val =
       tab.add("target", 0)
          .getEntry();
+
+    NetworkTable VisionTable = NetworkTableInstance.getDefault().getTable("Vision");
+
+  StructArrayPublisher<Pose2d> target_loc = VisionTable
+          .getStructArrayTopic("target_loc", Pose2d.struct).publish();
 
   /** Creates a new Shoot. */
   public Align(CommandSwerveDrivetrain drivetrain, SwerveRequest.FieldCentric drive, CommandXboxController driverController) {
@@ -67,19 +77,36 @@ public class Align extends Command {
   public void execute() {
 
 
-    target = targetHub // hub
-              .minus(
+    ChassisSpeeds speeds = ChassisSpeeds.fromRobotRelativeSpeeds(
+      m_drivetrain.getState().Speeds, m_drivetrain.getState().Pose.getRotation());
+
+    Transform2d onTheMoveOffset = new Transform2d(
+      speeds.vxMetersPerSecond * 0.0, // x offsets affect rpm not angle (TODO) 
+      (speeds.vyMetersPerSecond * 1.69), 
+      new Rotation2d(0.0));
+      
+    target = targetHub.plus(onTheMoveOffset.times(-1)); // if we are moving, we need to account for the estimated
+    
+    target_loc.set(new Pose2d[] {
+            new Pose2d(target.getX(), target.getY(), target.getRotation())
+        });
+
+    Transform2d robot_offset = target.minus(
                 new Pose2d(
                   m_drivetrain.getState().Pose.getX(),
                   m_drivetrain.getState().Pose.getY(), 
                   new Rotation2d(0.0)));
 
+    target = new Pose2d(robot_offset.getTranslation(), robot_offset.getRotation());
+
+    double targetAngle = Math.atan2(target.getY(), target.getX());
+
     double delta = MathUtil.angleModulus(
-      Math.atan2(target.getY(), target.getX()) - m_drivetrain.getState().Pose.getRotation().getRadians());
+      targetAngle - m_drivetrain.getState().Pose.getRotation().getRadians());
 
     delta_val.setDouble(delta);
     pose_val.setDouble(m_drivetrain.getState().Pose.getRotation().getRadians());
-    target_val.setDouble(Math.atan2(target.getY(), target.getX()));
+    target_val.setDouble(targetAngle);
 
     m_drivetrain.setControl(
       m_drive
