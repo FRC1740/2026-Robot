6931c5367ff3accc518b65dbadb4d8514bc1e384
Nikolas Daynard <Nkdaynard@gmail.com>
2026-06-06 10:54:34 -0400

    camn auto

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -133,7 +133,7 @@ public static final class VisionConstants {
 
     public static final Transform3d RobotToFrontCamera = new Transform3d(Units.inchesToMeters(-1), 0.0, Units.inchesToMeters(30.0), new Rotation3d(0.0, Units.degreesToRadians(15.0), 0.0));
     
-    public static final Transform3d RobotToBackRightCamera = new Transform3d(-.3, -.58/2, 19.5, new Rotation3d(0.0, 0.0, Math.PI + .52));
+    public static final Transform3d RobotToBackRightCamera = new Transform3d(-.3, -.22, 19.5, new Rotation3d(0.0, 0.0, Math.PI));
 
     public static final Transform3d RobotToFrontLeftCamera = new Transform3d(0.0, 0.25, Units.inchesToMeters(7.5), new Rotation3d(0.0, .52, Math.PI / 2.0));
     
--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -57,8 +57,8 @@ public class PhotonVision extends SubsystemBase {
         .getEntry();
     
     PhotonCamera frontCamera = new PhotonCamera("Front");
-    // PhotonCamera frontLeftCamera = new PhotonCamera("FrontLeft");
-    // PhotonCamera backRightCamera = new PhotonCamera("BackRight");
+    PhotonCamera frontLeftCamera = new PhotonCamera("Left");
+    PhotonCamera backRightCamera = new PhotonCamera("BackRight");
     PhotonCamera frontRightCamera = new PhotonCamera("FrontRight");
 
     PhotonPoseEstimator frontCameraEstimator = 
@@ -96,8 +96,8 @@ public class PhotonVision extends SubsystemBase {
     public PhotonVision() {
         is_teleop = false;
         cameras.add(new Camera(frontCamera, frontCameraEstimator, FrontCamPose));
-        // cameras.add(new Camera(frontLeftCamera, frontLeftCameraEstimator, FLCamPose));
-        // cameras.add(new Camera(backRightCamera, backRightCameraEstimator, BRCamPose));
+        cameras.add(new Camera(frontLeftCamera, frontLeftCameraEstimator, FLCamPose));
+        cameras.add(new Camera(backRightCamera, backRightCameraEstimator, BRCamPose));
         cameras.add(new Camera(frontRightCamera, frontRightCameraEstimator, FRCamPose));
     }
 
@@ -162,7 +162,7 @@ public class PhotonVision extends SubsystemBase {
 
                 camera.CamPose.setDoubleArray(m_poseArray);
                 
-                if (camera.camera.getName() != "BackRight" && camera.camera.getName() != "BackLeft" && camera.camera.getName() != "Front") {
+                if (camera.camera.getName() != "BackRight" && camera.camera.getName() != "Front") {
                     pose = new Pose2d(pose.getTranslation(), CommandSwerveDrivetrain.getInstance().getState().Pose.getRotation());
                 }else if (camera.camera.getName() == "Front") {
                     // pose = pose;
